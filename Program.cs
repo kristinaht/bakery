c3537eeecbfa8689d8a9fa9a0fc42de907daf13f
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-02-28 08:45:35 -0800

    create blank Bread and Pastry classes in Bakery.Models

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ namespace Bakery
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+        
         }
     }
 }
