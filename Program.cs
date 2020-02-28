89c2395ad9a96618cce5f078c35001666fb5d74f
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-02-28 08:51:17 -0800

    add welcome message to Program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace Bakery
     {
         static void Main()
         {
-        
+					Console.WriteLine(@"
+															Welcome to Pierre's Bakery! 
+				
+																MENU:
+															Item      	Price
+															Bread		$5
+															Pastry		$2");
         }
     }
 }
