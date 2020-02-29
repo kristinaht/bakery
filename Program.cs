61842fc799f4439f7d2346256c6389fe1d175b94
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-02-28 16:32:13 -0800

    move main menu logic outside Main()

--- Program.cs
+++ Program.cs
@@ -10,20 +10,7 @@ namespace Bakery
 					int breadPrice = 5;
 					Bread newBread = new Bread(breadPrice);
 					Pastry newPastry = new Pastry(2, "croissant");
-					// newBread.MainMenu();
-
-					Console.WriteLine(@"
-                          Welcome to Pierre's Bakery! 
-    
-                            		MENU:
-                          Item      	Price
-                          Bread		  $5
-                          Pastry		$2
-                                      ");
-					Console.WriteLine(@"We have bread fresh from the oven and delicous pastries. What would you like today?
-																[B] for just bread
-																[P] for just pastries
-																[BP] for both bread and pastries");
+					MainMenu();
 					string userInputMainPage = Console.ReadLine();
 					string userInputMainPageLower = userInputMainPage.ToLower();
 					if(userInputMainPageLower == "b")
@@ -41,11 +28,23 @@ namespace Bakery
 					}
 					else
 					{
-						// MainMenu();
+						MainMenu();
 					}
 
 				}
-				
+				public static void MainMenu()
+				{
+					Console.WriteLine(@"
+					Welcome to Pierre's Bakery! 
+						MENU:
+					Item      	Price
+					Bread		$5
+					Pastry		$2");
+					Console.WriteLine(@"We have bread fresh from the oven and delicous pastries. What would you like today?
+												[B] for just bread
+												[P] for just pastries
+												[BP] for both bread and pastries");
+				}
     }
 }
 
