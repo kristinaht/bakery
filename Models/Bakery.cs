178518020ad5d5c862ede24d682adff20a902bef
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-08 22:10:32 -0700

    modify setbreadprice

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -13,24 +13,14 @@ namespace Bakery.Models
 
     public int SetBreadPrice(int userInputBread)
     {
-      if(userInputBread %2 != 0 )
+
+      if(userInputBread >=3 )
       {
-        BreadPrice = BreadPrice * userInputBread; 
+        BreadPrice = userInputBread * 2/3 * BreadPrice;
       }
-      else{
-        BreadPrice = (BreadPrice * userInputBread)/2;
-      }
+      
       return BreadPrice;
     }
-    // public void CalculateBreadPrice()
-    // {
-    //   Console.WriteLine("How many loaves of bread would you like?");
-    //   string stringUserInputBread = Console.ReadLine();
-    //   int userInputBread = int.Parse(stringUserInputBread);
-  
-    //   SetBreadPrice(userInputBread);
-    //   Console.WriteLine("Your cost for the bread is $" + BreadPrice + ".");
-    // }
   }
 
   public class Pastry
@@ -45,7 +35,7 @@ namespace Bakery.Models
 
     public  int SetPastryPrice(int userInputPastry)
     {
-      PastryPrice = 2;
+      // PastryPrice = 2;
       if(userInputPastry == 3 )
       {
           PastryPrice = (userInputPastry * PastryPrice) - 1;
@@ -55,14 +45,5 @@ namespace Bakery.Models
       }
       return PastryPrice;
     }
-    // public void CalculatePastryPrice()
-    // {
-    //   int outputPastryPrice = PastryPrice;
-    //   Console.WriteLine("How many pieces of Pastry would you like?");
-    //   string stringUserInputPastry = Console.ReadLine();
-    //   int userInputPastry = int.Parse(stringUserInputPastry);
-    //   int totalPastryPrice = SetPastryPrice(userInputPastry);
-    //   Console.WriteLine("That will be $" + totalPastryPrice + ".");
-    // }
   }
-  }
+}
